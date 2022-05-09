cf0f7d671d4adf1864ae0855d614f4af5cab065c
Gideon Seifert <seifert.gideon@gmail.com>
2022-05-09 17:54:44 +0200

    Kommentar hinzugefügt:)

--- src/main/java/de/gideonseifert/mccourse/MCCourseMod.java
+++ src/main/java/de/gideonseifert/mccourse/MCCourseMod.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MCCourseMod implements ModInitializer {
-
+// Baum
 	public static final String MOD_ID = "mccourse";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
